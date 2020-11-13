f7940f00010a126760a7656071b06c1df4da3bf3
takaya-623 <takaya.suzuki.0623@gmail.com>
2020-11-13 06:01:48 +0000

    [finish]4-3 & [start]4-4

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c7e75341b5f3e2e39d3a253fbf7b5c8fdbe2ccac63b50a644114c929b0ada38ec3bf6044ddc7ba04361cd569930b50eb08502cbdb3976dfa147d4da8930626f9'
+
